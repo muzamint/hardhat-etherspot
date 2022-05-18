f440226a45421c6f34e1d953634f328fcbbe0e0e
Ming-der Wang <mingderwang@gmail.com>
2022-05-18 22:33:18 +0800

    use EOA

--- test/project.test.ts
+++ test/project.test.ts
@@ -18,8 +18,8 @@ describe('💥 Integration tests for the @muzamint/hardhat-etherspot plugin 💥
 
     it('Should works for sdk.updatePaymentHubDeposit()', async function () {
       const currentNetwork = NetworkNames.Etherspot
-      const hubPrivateKey = randomPrivateKey()
-      const privateKey = randomPrivateKey()
+      const hubPrivateKey = process.env.HUB_PRIVATE_KEY as WalletProviderLike // randomPrivateKey()
+      const privateKey = process.env.SENDER_PRIVATE_KEY as WalletProviderLike // randomPrivateKey()
       console.log('hubPrivateKey', hubPrivateKey)
       console.log('privateKey', privateKey)
 
@@ -40,7 +40,9 @@ describe('💥 Integration tests for the @muzamint/hardhat-etherspot plugin 💥
       const { accountAddress: hub } = hubState
       const { accountAddress: user } = userState
 
-      console.log('project key', process.env.TESTNETS_PROJECT_KEY)
+      console.log('🌈 hub wallet address:', hubState.accountAddress)
+      console.log('🌈 use wallet address:', userState.accountAddress)
+      console.log('🐤 project key', process.env.TESTNETS_PROJECT_KEY)
       const projects = await sdk.services.projectService.currentProject
       console.log('getProjects', projects)
       assert(ture, 'always ture')
@@ -75,24 +77,33 @@ describe('💥 Integration tests for the @muzamint/hardhat-etherspot plugin 💥
       const output3 = await hubSdk.getAccountBalances() // fails on Etherspot, but OK in Ropsten
       console.log('account balances3', output3.items[0].balance.toString())
 */
+      const newValue = Math.random()
+      const updateNewValue = newValue.toString()
+      const updateDeposit = (newValue / 2).toString()
+      console.log('✅ update payment hub a NewValue', updateNewValue)
       console.log(
-        'payment hub',
+        'payment hub to hubSdk',
         await hubSdk.updatePaymentHub({
-          liquidity: utils.parseEther('0.2'),
+          liquidity: utils.parseEther(updateNewValue),
+        }),
+      )
+
+      console.log(
+        'payment hub to sdk',
+        await sdk.updatePaymentHub({
+          liquidity: utils.parseEther(updateNewValue),
         }),
       )
       console.log(
-        'payment hub',
-        await sdk.updatePaymentHub({
-          liquidity: utils.parseEther('0.2'),
-        }),
+        '✅ update payment hub deposit half of the NewValue',
+        updateDeposit,
       )
       console.log('payment hub recipient deposit (updated)')
-      console.log('starging sdk.updatePaymentHubDeposit()...')
+      console.log('starging sdk.updatePaymentHubDeposit()...💥')
       await sdk
         .updatePaymentHubDeposit({
           hub: user,
-          totalAmount: utils.parseEther('0.05'),
+          totalAmount: utils.parseEther(updateDeposit),
         })
         .then((r) => {
           console.log('Result -> ', r)
